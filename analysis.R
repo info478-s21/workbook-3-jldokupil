9a26452c4aa31433326791fc7b1bf8792d434aca
jldokupil <jdokupil@uw.edu>
2021-05-03 16:06:59 -0700

    linted code

--- analysis.R
+++ analysis.R
@@ -1,9 +1,12 @@
 # Analysis
+# Workbook 3
+# Name: John-Luke Dokupil
 
 # Set up - make sure to set your working directory using RStudio
 library(tidyr)
 library(dplyr)
 library(ggplot2)
+library(lintr)
 
 # Create the `charts/` directory (you can do this from R!)
 dir.create("charts", showWarnings = FALSE)
@@ -13,17 +16,16 @@ dir.create("charts", showWarnings = FALSE)
 health_data <- read.csv("./data/prepped/all_data.csv")
 View(health_data)
 
-
 # Are HALE and life expectancy correlated?
 # - Plot 2016 life expectancy against 2016 HALE. Save the graph to `charts/`
 # - Compute the correlation between 2016 life expectancy against 2016 HALE
-data_2016 <- health_data %>% 
+data_2016 <- health_data %>%
   filter(year == 2016)
 
 ggplot(data_2016) +
-  geom_point(mapping = aes(x = le , y = hale)) +
+  geom_point(mapping = aes(x = le, y = hale)) +
   labs(
-    title = "Life Expectancy vs HALE", 
+    title = "Life Expectancy vs HALE",
     x = "Life Expectancy",
     y = "HALE"
   )
@@ -32,14 +34,13 @@ ggsave("charts/le_hale_graph.png")
 
 cor(data_2016$le, data_2016$hale)
 
-
 # Are HALE and DALYs correlated?
 # - Plot 2016 HALE against 2016 DALYs. Save the graph to `charts/`
 # - Compute the correlation between 2016 HALE and DALYs
 ggplot(data_2016) +
   geom_point(mapping = aes(x = dalys, y = hale)) +
   labs(
-    title = "DALYs vs HALE", 
+    title = "DALYs vs HALE",
     x = "DALYs",
     y = "HALE"
   )
@@ -48,8 +49,7 @@ ggsave("charts/dalys_hale_graph.png")
 
 cor(data_2016$dalys, data_2016$hale)
 
-
-# As people live longer, do they live healthier lives 
+# As people live longer, do they live healthier lives
 # (i.e., is a smaller fraction of life spent in poor health)?
 # Follow the steps below to attempt to answer this question.
 
@@ -57,16 +57,14 @@ cor(data_2016$dalys, data_2016$hale)
 # This will create `hale_2016`, `hale_1990`, `le_2016`, etc.
 # To do this, I suggest that you use the `pivot` function in the new
 # tidyverse release:https://tidyr.tidyverse.org/articles/pivot.html#wider
-data_wide <- health_data %>% 
+data_wide <- health_data %>%
   pivot_wider(names_from = year,
               values_from = c(hale, le, dalys))
 
-
 # Create columns to store the change in life expectancy, and change in hale
-data_wide <- data_wide %>% 
+data_wide <- data_wide %>%
   mutate(hale_diff = hale_2016 - hale_1990,
          le_diff = le_2016 - le_1990)
-
 
 # Plot the *change in hale* against the *change in life expectancy*
 # Add a 45 degree line (i.e., where x = y), and save the graph to `charts/`
@@ -74,7 +72,7 @@ data_wide <- data_wide %>%
 ggplot(data_wide) +
   geom_point(mapping = aes(x = le_diff, y = hale_diff)) +
   labs(
-    title = "Life Expectancy difference vs HALE difference", 
+    title = "Life Expectancy difference vs HALE difference",
     x = "Change in Life Expectancy",
     y = "Change in HALE"
   ) +
@@ -84,5 +82,6 @@ ggplot(data_wide) +
 
 ggsave("charts/change_plot.png")
 
-# This chart shows that, despite how close HALE and Life Expectancy are to each other,
+# This chart shows that, despite how close HALE and
+# Life Expectancy are to each other,
 # HALE is on average, lower than Life Expectancy.
